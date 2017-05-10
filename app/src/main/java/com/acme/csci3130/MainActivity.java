b096d4d989eb12b8e5a19b208a2c69d84618768e
Matt <matthew.smith@dal.ca>
2017-05-10 20:56:37 -0300

    newUIElements

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,37 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button btn;
+    private EditText txt;
+    private TextView helloTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+
+        txt = (EditText) findViewById(R.id.editText2);
+        helloTxt = (TextView) findViewById(R.id.helloText);
+    }
+
+    @Override
+    public void onClick(View v) {
+        helloTxt.setText(txt.getText());
+        //btn.setText(txt.getText());
     }
 }
+
+
+
